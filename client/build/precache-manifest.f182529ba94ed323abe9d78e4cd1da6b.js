f40c459182e045487b7895dad253da0d065411bf
Tanya Ivato <tetiana.ivato@gmail.com>
2021-02-07 14:53:07 +0200

    Added draft public folder

---
+++ client/build/precache-manifest.f182529ba94ed323abe9d78e4cd1da6b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4d155826ff1dd882487d715b6619515",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f792f8488beb837f0d16",
+    "url": "/static/css/main.eea09449.chunk.css"
+  },
+  {
+    "revision": "88a1f68ea0666da63b2c",
+    "url": "/static/js/2.35e4ae11.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.35e4ae11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f792f8488beb837f0d16",
+    "url": "/static/js/main.02b81852.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
