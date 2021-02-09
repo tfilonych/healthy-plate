078ba9ed94e4ee39bbff8b2198712d5b1fcd0f6b
Tanya Ivato <tetiana.ivato@gmail.com>
2021-02-09 20:06:59 +0200

    Minor fix

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
-import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
-import useRoutes from "./routes";
-import useAuth from "./hooks/auth.hook";
-import AuthContext from "./context/AuthContext";
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import useRoutes from './routes';
+import useAuth from './hooks/auth.hook';
+import AuthContext from './context/AuthContext';
 import Header from './components/Header';
 import Footer from './components/Footer';
 
