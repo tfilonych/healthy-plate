640f2bb17099da509dbd09975bc8fde4e9b47069
Tanya Ivato <tetiana.ivato@gmail.com>
2024-03-11 21:56:36 +0200

    Minor fix

--- render/renderClient.js
+++ render/renderClient.js
@@ -1,31 +1,28 @@
 import React from 'react';
-import App from './App';
+import { renderToPipeableStream } from 'react-dom/server';
+import { StaticRouter } from 'react-router-dom/server';
+import AppSSR from '../client/src/AppSSR';
+import { bootstrapCSS, bootstrapScripts } from '../bootstrappedFiles';
 
-// eslint-disable-next-line react/prop-types
-const AppSSR = ({ bootStrapCSS }) => {
-  return (
-    <html>
-    <head>
-      <meta charSet='utf-8' />
-      <meta name='viewport' content='width=device-width, initial-scale=1' />
-      <base href='/' />
-      <title>SSR React 18 example</title>
-      <link rel='stylesheet' href='../../public/index.css' />
-      <link rel='stylesheet'
-            href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css'
-      />
-      {
-        // eslint-disable-next-line react/prop-types
-        bootStrapCSS.map(cssPath => <link key={cssPath} rel='stylesheet' href={cssPath}></link>)
+const renderClient = (req, res) => {
+  let didError = false;
+  const stream = renderToPipeableStream(
+    <StaticRouter location={req.url} context={req.params}>
+      <AppSSR bootStrapCSS={bootstrapCSS} />
+    </StaticRouter>,
+    {
+      bootstrapScripts,
+      onShellReady: () => {
+        res.statusCode = didError ? 500 : 200;
+        res.setHeader('Content-type', 'text/html');
+        stream.pipe(res);
+      },
+      onError: (error) => {
+        didError = true;
+        console.log('Error', error);
       }
-    </head>
-    <body>
-    <div id='root'>
-      <App />
-    </div>
-    </body>
-    </html>
+    }
   );
 };
 
-export default AppSSR;
+export default renderClient;
